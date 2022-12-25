8c03dcf8e36839670196acf9353bf264d0e7f568
Aleksey-Kudryavtsev <aleksey.kudryavtsev@gmail.com>
2022-12-25 21:28:45 +0300

    Returning font-size 15px in the css and removing it in JS and removing the extra meta tag (only leaving the veiwport tag)
    See https://developer.mozilla.org/en-US/docs/Web/HTML/Viewport_meta_tag

--- weather/index.js
+++ weather/index.js
@@ -23,8 +23,7 @@ callWs = function () {
           && m.coordinates[1] > 53.83023 && m.coordinates[1] < 53.98983) {
           let li = document.createElement('li');
           let date = new Date(m.description.date);
-          li.innerText = `${date.getHours()}:${date.getMinutes()} ${m.description.temperature} ${m.description.water} ${m.description.wind}`;
-          li.style = 'font-size: 15px';
+          li.innerText = `${date.getHours()}:${date.getMinutes()} ${m.description.temperature} ${m.description.water} ${m.description.wind}`;          
           resultElement.appendChild(li);
         }
       }
