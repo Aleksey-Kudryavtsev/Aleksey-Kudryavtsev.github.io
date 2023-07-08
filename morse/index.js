2b1ab0bebd8ff326c37d284d70bdefdac9b2542e
Aleksey-Kudryavtsev <aleksey.kudryavtsev@gmail.com>
2023-07-09 01:58:45 +0300

    Adding a new input for entering the answer and Alt+P combination for playing the sound

--- morse/index.js
+++ morse/index.js
@@ -1,13 +1,13 @@
 const morseCodes = {
-    'A': '.-', 'B': '-...', 'C': '-.-.', 'D': '-..', 'E': '.', 'F': '..-.', 'G': '--.',
-    'H': '....', 'I': '..', 'J': '.---', 'K': '-.-', 'L': '.-..', 'M': '--', 'N': '-.',
-    'O': '---', 'P': '.--.', 'Q': '--.-', 'R': '.-.', 'S': '...', 'T': '-',
-    'U': '..-', 'V': '...-', 'W': '.--', 'X': '-..-', 'Y': '-.--', 'Z': '--..',
-    '0': '-----', '1': '.----', '2': '..---', '3': '...--', '4': '....-', '5': '.....',
-    '6': '-....', '7': '--...', '8': '---..', '9': '----.'
-  };
+  'A': '.-', 'B': '-...', 'C': '-.-.', 'D': '-..', 'E': '.', 'F': '..-.', 'G': '--.',
+  'H': '....', 'I': '..', 'J': '.---', 'K': '-.-', 'L': '.-..', 'M': '--', 'N': '-.',
+  'O': '---', 'P': '.--.', 'Q': '--.-', 'R': '.-.', 'S': '...', 'T': '-',
+  'U': '..-', 'V': '...-', 'W': '.--', 'X': '-..-', 'Y': '-.--', 'Z': '--..',
+  '0': '-----', '1': '.----', '2': '..---', '3': '...--', '4': '....-', '5': '.....',
+  '6': '-....', '7': '--...', '8': '---..', '9': '----.'
+};
 
-const supportedCharacters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'.split('').sort((c1,c2) => morseCodes[c1].length > morseCodes[c2].length ? 1 : -1).join('');
+const supportedCharacters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'.split('').sort((c1, c2) => morseCodes[c1].length > morseCodes[c2].length ? 1 : -1).join('');
 
 function saveCheckboxState(character) {
   const checkbox = document.getElementById(`checkbox-${character}`);
@@ -31,15 +31,15 @@ function loadCheckboxState(character) {
     const checkbox = document.createElement('input');
     checkbox.type = 'checkbox';
     checkbox.id = `checkbox-${character}`;
-    checkbox.onchange = () => saveCheckboxState(character);        
-    
+    checkbox.onchange = () => saveCheckboxState(character);
+
 
     label.appendChild(checkbox);
     label.appendChild(document.createTextNode(character + ' [' + morseCodes[character] + ']'));
 
     checkboxesContainer.appendChild(label);
     checkboxesContainer.appendChild(document.createElement("br"));
-    
+
     loadCheckboxState(character);
   }
 })();
@@ -59,11 +59,11 @@ function generateRandomText() {
   let numberOfChars = 10;
   for (let i = 0; i < numberOfChars; i++) {
     const randomIndex = Math.floor(Math.random() * selectedCharacters.length);
-    randomText += selectedCharacters[randomIndex];        
+    randomText += selectedCharacters[randomIndex];
   }
-  
-  if(Math.random()>0.5) {
-    var spacePosition = Math.floor(Math.random()*(numberOfChars-1) + 1);
+
+  if (Math.random() > 0.5) {
+    var spacePosition = Math.floor(Math.random() * (numberOfChars - 1) + 1);
     let splitText = randomText.split('');
     splitText.splice(spacePosition, 0, ' ');
     randomText = splitText.join('');
@@ -74,11 +74,11 @@ function generateRandomText() {
 
 
 function sleep(ms) {
-return new Promise(resolve => setTimeout(resolve, ms));
+  return new Promise(resolve => setTimeout(resolve, ms));
 }
 
 async function convertAndPlay() {
-  
+
 
   let inputText = document.getElementById('inputText').value.toUpperCase();
   const ditDuration = 100; // milliseconds
@@ -98,7 +98,7 @@ async function convertAndPlay() {
     let fadingDuration = 30;
     await sleep(duration - fadingDuration);
     gainNode.gain.setValueAtTime(gainNode.gain.value, audioContext.currentTime);
-    gainNode.gain.exponentialRampToValueAtTime(0.0001, audioContext.currentTime + fadingDuration/1000.0);
+    gainNode.gain.exponentialRampToValueAtTime(0.0001, audioContext.currentTime + fadingDuration / 1000.0);
     await sleep(fadingDuration);
     oscillator.stop();
   }
@@ -110,25 +110,25 @@ async function convertAndPlay() {
         await playBeep(ditDuration);
       }
       else if (code[i] === '-') {
-        await playBeep(dahDuration);            
+        await playBeep(dahDuration);
       }
       else if (code[i] === '/') {
         await sleep(letterSpaceDuration);
       }
-      else if(code[i] === '[') {
+      else if (code[i] === '[') {
         await sleep(wordSpaceDuration);
-      }                    
+      }
     }
   }
 
   let morseCode = '';
-  inputText = inputText.trim().replace(/[^a-z0-9 ]/gi, '').split('').join('/').replace(/\/\s+\//g,'[');
+  inputText = inputText.trim().replace(/[^a-z0-9 ]/gi, '').split('').join('/').replace(/\/\s+\//g, '[');
   for (let i = 0; i < inputText.length; i++) {
-    const char = inputText[i];   
-    if(char in morseCodes) {
-       morseCode += morseCodes[char];        
+    const char = inputText[i];
+    if (char in morseCodes) {
+      morseCode += morseCodes[char];
     } else {
-       morseCode += char;
+      morseCode += char;
     }
   }
   playMorseCode(morseCode);
@@ -138,3 +138,11 @@ function showHideToggle() {
   let inputElem = document.getElementById('inputText');
   inputElem.hidden = !inputElem.hidden;
 }
+
+function onKeyUpBody(e) {
+  if (e.altKey && e.code === "KeyP") {
+    convertAndPlay();
+  }
+}
+
+document.body.addEventListener("keyup", (e) => onKeyUpBody(e));
